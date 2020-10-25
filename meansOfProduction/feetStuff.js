aeab71ad25438c146f78b23c8faa1eeaab164d48
Schitte <schitteindustries@gmail.com>
2020-10-25 08:08:35 -0500

    boundary and link

--- meansOfProduction/feetStuff.js
+++ meansOfProduction/feetStuff.js
@@ -6,7 +6,7 @@ var container = document.createElement("div");
 container.className = "SI_FEET_STUFF";
 container.innerHTML =
 	"<div class='SI_FEET_THUMB_NAIL'></div>" +
-	"<h1 class='SI_FEET_BIG_TOE'>Made with $ by Schitte Industries</h1>"
+	"<h1 class='SI_FEET_BIG_TOE'>Made with $ by <a target='__blank' href='https://schitte.github.io'>Schitte Industries</a></h1>"
 ;
 
 document.addEventListener('DOMContentLoaded', function () {
