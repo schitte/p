d6b0830cd369abcd8502a3c8780d682a9f7394f4
Schitte <schitteindustries@gmail.com>
2020-10-25 04:31:32 -0500

    preliminary finish footie

--- meansOfProduction/feetStuff.js
+++ meansOfProduction/feetStuff.js
@@ -4,7 +4,10 @@ console.log("Footer? I hardly know her");
 
 var container = document.createElement("div");
 container.className = "SI_FEET_STUFF";
-container.innerHTML = "<h1 class='SI_FEET_BIG_TOE'>Brought to you by Schitte Industries</h1>";
+container.innerHTML =
+	"<div class='SI_FEET_THUMB_NAIL'></div>" +
+	"<h1 class='SI_FEET_BIG_TOE'>Made with $ by Schitte Industries</h1>"
+;
 
 document.addEventListener('DOMContentLoaded', function () {
 	document.body.appendChild(container);
