c60e30aeb3860b8723aff91f8a467caec56780a2
Schitte <schitteindustries@gmail.com>
2020-10-25 01:25:06 -0500

    Add thumb nail to the footie

---
+++ meansOfProduction/feetStuff.js
@@ -0,0 +1,11 @@
+//footer? I hardly know her
+
+console.log("Footer? I hardly know her");
+
+var container = document.createElement("div");
+container.className = "SI_FEET_STUFF";
+container.innerHTML = "<h1 class='SI_FEET_BIG_TOE'>Brought to you by Schitte Industries</h1>";
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.body.appendChild(container);
+});
--- meansOfProduction/feetstuff.js
+++
@@ -1,5 +0,0 @@
-//footer? I hardly know her
-
-var container = document.createElement("div");
-container.className = "SI_FEET_STUFF";
-container.innerHTML = "<p>Brought to you by Schitte Industries</p>";
