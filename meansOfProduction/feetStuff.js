9864efbb4863cf334de89b9c6415266ebb683158
Schitte <schitteindustries@gmail.com>
2020-12-24 23:40:29 -0600

    update footie and remove brown colors on pages

--- meansOfProduction/feetStuff.js
+++ meansOfProduction/feetStuff.js
@@ -6,7 +6,9 @@ var container = document.createElement("div");
 container.className = "SI_FEET_STUFF";
 container.innerHTML =
 	"<div class='SI_FEET_THUMB_NAIL'></div>" +
-	"<h1 class='SI_FEET_BIG_TOE'>Made with $ by <a target='__blank' href='https://schitte.github.io'>Schitte Industries</a></h1>"
+	"<h1 class='SI_FEET_BIG_TOE'>Made with $ by <a target='__blank' href='https://schitte.github.io'>Schitte Industries</a></h1>" +
+	"<h1 class='SI_FEET_PINKY_TOE'><a target='__blank' href='https://www.patreon.com/schitte'>Please Feed Us</a></h1>"
+
 ;
 
 document.addEventListener('DOMContentLoaded', function () {
